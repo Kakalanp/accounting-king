e113c52d7dc679291f0ad557f0046244555c9e07
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-28 17:27:12 -0500

    Adding categories controller and route

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,25 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Group.includes(:entities).where(user: current_user)
+  end
+
+  def new
+    @category = Group.new
+  end
+
+  def create
+    category = Group.new(category_params)
+
+    if category.save
+      redirect_to categories_path, notice: 'Category added successfully'
+    else
+      render new, alert: 'Category could not be added successfully'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:user, :name, :image).reverse_merge(user: current_user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :splash_screen
+  resources :splash_screen, :categories
 end
