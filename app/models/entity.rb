dd468e8a7e00261559e907eb677b5888fec8709c
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-29 11:06:26 -0500

    Bugfix to entities, small detail addition

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,4 +1,4 @@
 class Entity < ApplicationRecord
   belongs_to :user, class_name: 'User'
-  has_many :groups, foreign_key: :user_id, class_name: 'Group'
+  belongs_to :group, class_name: 'Group'
 end
